a5315941aeff3f6d263acfd615cceee13fde5cc3
y7ya <yhya9813@gmail.com>
2024-04-17 03:53:18 +0300

    implement celery worker container

--- api/scan/views.py
+++ api/scan/views.py
@@ -1,12 +1,13 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from hunter.utils import get_up_hosts
+from .tasks import add
 
 @api_view(['POST'])
 def up_hosts(request):
     ips = []
     if request.data.get('ips', None) is not None:
         ips = request.data.get('ips').split('\n')
-    
-    get_up_hosts(ips)
+        add.delay(1,2)        
+    # get_up_hosts(ips)
     return Response({'status':"start checking uphosts",'xx':ips})
